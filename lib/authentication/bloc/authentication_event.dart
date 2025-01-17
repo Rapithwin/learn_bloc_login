fa73e869f97d02228769c0284be2b616c489419a
unknown <khandanamir7@gmail.com>
2025-01-17 20:19:02 +0330

    Events added

--- lib/authentication/bloc/authentication_event.dart
+++ lib/authentication/bloc/authentication_event.dart
@@ -1,1 +1,11 @@
+part of 'authentication_bloc.dart';
 
+sealed class AuthenticationEvent {
+  const AuthenticationEvent();
+}
+
+/// Initail event that notifies teh bloc to subscribe to the `AuthenticationStatus` stream.
+final class AuthenticationSubscriptionRequested extends AuthenticationEvent {}
+
+/// Notifies the bloc of a user logout action.
+final class AuthenticationLogoutPressed extends AuthenticationEvent {}
