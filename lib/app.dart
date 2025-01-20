04d1976721bb4e888e225d26d6556866f9918eeb
unknown <khandanamir7@gmail.com>
2025-01-20 13:56:51 +0330

    Created app.dart and added to main

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
