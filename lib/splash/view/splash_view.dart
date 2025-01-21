5590d11ebc3528f0978eca5cfacba427d69544a3
unknown <khandanamir7@gmail.com>
2025-01-21 18:52:27 +0330

    splash_view UI

--- lib/splash/view/splash_view.dart
+++ lib/splash/view/splash_view.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static Route<void> route() => MaterialPageRoute<void>(
+        builder: (context) => SplashPage(),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
