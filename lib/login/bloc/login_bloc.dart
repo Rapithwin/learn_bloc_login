640d0e0c57bad94eb15df38e7aab1dd1f114ded6
unknown <khandanamir7@gmail.com>
2025-01-21 21:45:09 +0330

    Imports and part

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -1,1 +1,6 @@
+import 'package:bloc_login/login/models/models.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
 
+part 'login_state.dart';
+part 'login_event.dart';
