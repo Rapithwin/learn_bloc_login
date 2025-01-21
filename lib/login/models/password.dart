0be57808cc9a3eadb641417c0521ad758cf979e3
unknown <khandanamir7@gmail.com>
2025-01-21 20:57:15 +0330

    Password model implemented

--- lib/login/models/password.dart
+++ lib/login/models/password.dart
@@ -1,1 +1,15 @@
+import 'package:formz/formz.dart';
 
+enum PasswordValidationError { empty, charLen }
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.pure() : super.pure("");
+  const Password.dirty([super.value = ""]) : super.dirty();
+
+  @override
+  PasswordValidationError? validator(String value) {
+    if (value.isEmpty) return PasswordValidationError.empty;
+    if (value.length < 8) return PasswordValidationError.charLen;
+    return null;
+  }
+}
