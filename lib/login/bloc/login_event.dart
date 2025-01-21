65f81fc081cf2ffa1e0b3a6ecfd909071279dfc5
unknown <khandanamir7@gmail.com>
2025-01-21 21:33:21 +0330

    Login events implemented

--- lib/login/bloc/login_event.dart
+++ lib/login/bloc/login_event.dart
@@ -1,1 +1,29 @@
+import 'package:equatable/equatable.dart';
 
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+  @override
+  List<Object?> get props => [];
+}
+
+final class LoginUsernameChanged extends LoginEvent {
+  final String username;
+
+  const LoginUsernameChanged(this.username);
+  @override
+  List<Object?> get props => [username];
+}
+
+final class LoginPasswordChanged extends LoginEvent {
+  final String password;
+
+  const LoginPasswordChanged(this.password);
+  @override
+  List<Object?> get props => [password];
+}
+
+class LoginSubmitted extends LoginEvent {
+  const LoginSubmitted();
+  @override
+  List<Object?> get props => [];
+}
