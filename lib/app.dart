7d395c67ac63c47cb8c2841947cd9b9b0310f843
unknown <khandanamir7@gmail.com>
2025-01-21 17:15:20 +0330

    App widget implemented

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,8 @@
+import 'package:authentication_repository/authenticationi_repository.dart';
+import 'package:bloc_login/authentication/bloc/authentication_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -8,9 +12,34 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  late final AuthenticationRepository _authenticationRepository;
+  late final UserRepository _userRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticationRepository = AuthenticationRepository();
+    _userRepository = UserRepository();
+  }
+
+  @override
+  void dispose() {
+    _authenticationRepository.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: _authenticationRepository,
+          userRepository: _userRepository,
+        ),
+        child: const AppView(),
+      ),
+    );
   }
 }
 
