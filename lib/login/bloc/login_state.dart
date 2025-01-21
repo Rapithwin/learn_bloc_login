96528e5b2ae81b1f21c6c0ea28cfa7f59e03acfb
unknown <khandanamir7@gmail.com>
2025-01-21 21:44:51 +0330

    Login state implemented

--- lib/login/bloc/login_state.dart
+++ lib/login/bloc/login_state.dart
@@ -1,1 +1,32 @@
+part of 'login_bloc.dart';
 
+final class LoginState extends Equatable {
+  final Username username;
+  final Password password;
+  final bool isValid;
+  final FormzSubmissionStatus status;
+
+  const LoginState({
+    this.username = const Username.pure(),
+    this.password = const Password.pure(),
+    this.isValid = false,
+    this.status = FormzSubmissionStatus.initial,
+  });
+
+  LoginState copyWith(
+    Username? username,
+    Password? password,
+    bool? isValid,
+    FormzSubmissionStatus? status,
+  ) {
+    return LoginState(
+      username: username ?? this.username,
+      password: password ?? this.password,
+      isValid: isValid ?? this.isValid,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object?> get props => [username, password, status];
+}
