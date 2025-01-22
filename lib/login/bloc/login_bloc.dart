b36e7aac5c705e70eb13508f3036a0d46bdcfde0
unknown <khandanamir7@gmail.com>
2025-01-22 19:35:14 +0330

    Login bloc class created with _onUsernameChanged method

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -1,6 +1,35 @@
+import 'package:authentication_repository/authenticationi_repository.dart';
+import 'package:bloc/bloc.dart';
 import 'package:bloc_login/login/models/models.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
 
 part 'login_state.dart';
 part 'login_event.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc({required AuthenticationRepository authenticationRepository})
+      : _authenticationRepository = authenticationRepository,
+        super(const LoginState()) {
+    on<LoginUsernameChanged>(_onUsernameChanged);
+    on<LoginPasswordChanged>(_onPasswordChanged);
+    on<LoginSubmitted>(_onSubmitted);
+  }
+
+  final AuthenticationRepository _authenticationRepository;
+
+  void _onUsernameChanged(
+    LoginUsernameChanged event,
+    Emitter<LoginState> emit,
+  ) {
+    final username = Username.dirty(event.username);
+    emit(
+      state.copyWith(
+        username: username,
+        isValid: Formz.validate(
+          [state.password, username],
+        ),
+      ),
+    );
+  }
+}
