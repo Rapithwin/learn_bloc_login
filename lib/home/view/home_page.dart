7d62dc7187944a109e7ad474af475a3a69fca53f
unknown <khandanamir7@gmail.com>
2025-01-25 17:18:50 +0330

    _UserId widget for displaying the user id implemented

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_login/authentication/bloc/authentication_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,5 +12,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class _UserId extends StatelessWidget {
+  const _UserId();
+
+  @override
+  Widget build(BuildContext context) {
+    final userId =
+        context.select((AuthenticationBloc bloc) => bloc.state.user.id);
+    return Text("User ID: $userId");
   }
 }
